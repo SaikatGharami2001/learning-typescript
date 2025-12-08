b21b3dd44063ef5671c88e10f09f142da6ef0afe
SaikatGharami2001 <saikatgharami2@gmail.com>
2025-12-08 11:02:48 +0530

    Learnt Object types

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,63 @@
-function add(a: number, b: number): number {
-  return a + b;
-}
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  inStock: boolean;
+};
 
-const c = add(3, 3);
-console.log(c); // 6
+const obj1: Product = {
+  id: 1,
+  title: "Mobile phone",
+  price: 499,
+  inStock: true,
+};
+
+const obj2: Product = {
+  id: 2,
+  title: "Mobile phone",
+  price: 499,
+  inStock: true,
+};
+
+const obj3: Product = {
+  id: 3,
+  title: "Mobile phone",
+  price: 499,
+  inStock: true,
+};
+
+const items: Product[] = [obj1, obj2];
+items.push(obj3);
+items.push(obj3);
+
+console.log(items);
+
+type Count1 = Record<"likes" | "views" | "shares", number>;
+
+// So this is shortand for
+
+type Count2 = Record<
+  "I" | "want" | "to" | "settle" | "in" | "Turkey",
+  string | number
+>;
+
+const hey: Count2 = {
+  I: 3,
+  want: "Yes",
+  to: "Yes",
+  settle: "Yes",
+  in: "Yes",
+  Turkey: "Yes",
+};
+
+type sign = { [index: string]: number };
+
+const mySign: sign = {
+  Saikat: 1,
+  Sagar: 1,
+};
+
+mySign.Rahul = 9;
+mySign["real"] = 90;
+
+console.log(mySign);
